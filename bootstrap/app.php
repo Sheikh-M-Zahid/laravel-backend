8be1558d2ae1b987cef372c328ae2a4eee09e28a
Md. Zahid Hossain <2024-1-60-074@std.ewubd.edu>
2026-08-16 09:45:31 +0600

    Add trustProxies middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\EnsureSuperAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
+
         $middleware->alias([
             'role' => EnsureRole::class,
             'super_admin' => EnsureSuperAdmin::class,
